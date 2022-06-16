896ae8dcae49eec5a26d364b4fe56369845ebd2d
Manoj1306 <80400735+Manoj1306@users.noreply.github.com>
2022-06-16 12:24:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,11 @@ fruits_to_show= my_fruit_list.loc[fruits_selected]
 #display table on the page
 streamlit.dataframe(fruits_to_show)
 
+import requests
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruitvice_response)
+  #fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
